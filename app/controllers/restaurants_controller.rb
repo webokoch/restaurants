b255e51e695b317667e0fd298c0a19c4a3da0d34
Stefan Koch <webokoch@gmx.de>
2021-08-31 09:48:11 +0200

    added restaurant new & create action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,22 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create([restaurant_params])
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :food, :description, :location)
+  end
 end

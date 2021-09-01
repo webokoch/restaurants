684a4c9c230eebb244391ac620579744a3ba61a5
Stefan Koch <webokoch@gmx.de>
2021-09-01 11:28:32 +0200

    added new action and view to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.create(review_params)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end

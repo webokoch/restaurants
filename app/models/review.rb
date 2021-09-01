a4958c17ea4b4d3d68da7b6bbe1545c3bf89263f
Stefan Koch <webokoch@gmx.de>
2021-09-01 11:36:26 +0200

    added collection to rating in form

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, presence: true, inclusion: 1..5
+  validates :rating, presence: true, inclusion: { in: 1..5 }
   validates :content, presence: true, length: { maximum: 500 }
 end
